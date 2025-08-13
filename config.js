34698ce68b726f4f014fe2921b4a08e4233d4231
districell <embersleep7@gmail.com>
2025-08-13 12:02:08 -0300

    Update config.js

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ const FB_CONFIG = {
   apiKey: "AIzaSyBFBD_aL8FiDUXZAF73pCYIB4fprUfm6u8",
   authDomain: "districell-cea1d.firebaseapp.com",
   projectId: "districell-cea1d",
-  storageBucket: "districell-cea1d.appspot.com",
+  storageBucket: "districell-cea1d.firebasestorage.app",
   messagingSenderId: "43060721015",
   appId: "1:43060721015:web:cc119a7c35fb86703e5d72",
   measurementId: "G-CSHTJDJ5RX"
